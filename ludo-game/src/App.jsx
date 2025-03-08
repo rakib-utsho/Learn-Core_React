7c2cfba515300328bb66edfcb13bad8ea1eff4a2
rakib-utsho <rakibutsho1920@gmail.com>
2025-03-09 00:27:29 +0600

    updaTE

--- ludo-game/src/App.jsx
+++ ludo-game/src/App.jsx
@@ -2,11 +2,14 @@ import "./App.css";
 import LudoBoard from "./LudoBoard";
 import TodoList from "./TodoList";
 import Lottery from "./Lottery";
+import TicketNum from "./TicketNum";
+import Ticket from "./Ticket";
 
 function App() {
   return (
     <>
-      <Lottery />
+      <Ticket ticket={[0, 1, 2]} />
+      <Ticket ticket={[5, 6, 7, 8, 9]} />
     </>
   );
 }
